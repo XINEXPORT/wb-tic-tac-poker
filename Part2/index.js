fc7215e558051c6a858ee8e40838f667222a85ce
Christine Hoang <christinehoangt@gmail.com>
2023-11-11 10:03:24 -0600

    initcommit

--- Part2/index.js
+++ Part2/index.js
@@ -1,7 +1,9 @@
-const card = document.querySelector ('card');
-const cardStyle = document.querySelector ('cardStyle');
-const cardButton = document.querySelector ('cardButton');
+const idInput = document.querySelector('#idInput');
+const colorInput = document.querySelector('#colorInput');
 
 function setCard(){
-    
+    const card = document.querySelector(`#${idInput.value}`);
+    card.style.color = colorInput.value;
 }
+
+document.querySelector ('#editCardButton').addEventListener ('click', setCard);
